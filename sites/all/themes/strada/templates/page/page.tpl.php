6b14c333ee159f7fd5ee43ce334b5dea0354b6c9
ntym4ek <mega_comp@mail.ru>
2019-10-23 09:34:57 +0300

    "оцените нас" в подвале

--- sites/all/themes/strada/templates/page/page.tpl.php
+++ sites/all/themes/strada/templates/page/page.tpl.php
@@ -266,12 +266,14 @@
             </ul>
         </div>
         <div class="f-social">
-            <h5>JOY в социальных сетях</h5>
+            <h5>Оцените<br />нас</h5>
             <div class="social-links">
                 <a class="vk" href="https://vk.com/joy_garden" rel="nofollow noopener" target="_blank" title="ВКонтакте"><i class="fab fa-vk" aria-hidden="true"></i></a>
                 <a class="fb" href="http://www.facebook.com/kccc.ru" rel="nofollow noopener" target="_blank" title="Facebook"><i class="fab fa-facebook" aria-hidden="true"></i></a>
                 <a class="in" href="https://www.instagram.com/joy_info" rel="nofollow noopener" target="_blank" title="Instagram"><i class="fab fa-instagram" aria-hidden="true"></i></a>
             </div>
+            <div>Горячая линия Интернет-магазина</div>
+            <div class="support-phone"><a href="tel:88005502885">8-800-550-28-85</a></div>
         </div>
     </div>
     <div class="f-scripts">
