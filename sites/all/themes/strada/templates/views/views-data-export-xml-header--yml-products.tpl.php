dac01ff8462847fc656893b4baa3c1a11e837a01
ntym4ek <mega_comp@mail.ru>
2020-03-19 10:43:20 +0300

    правки

--- sites/all/themes/strada/templates/views/views-data-export-xml-header--yml-products.tpl.php
+++ sites/all/themes/strada/templates/views/views-data-export-xml-header--yml-products.tpl.php
@@ -3,7 +3,7 @@
 <!DOCTYPE yml_catalog SYSTEM "shops.dtd">
 <yml_catalog date="<?php echo date('Y-m-d h:i'); ?>">
   <shop>
-    <name><?php echo variable_get('site_name', 'Drupal'); ?></name>
+    <name>JOY-MAGAZIN - <?php echo variable_get('site_name', 'Drupal'); ?></name>
     <company>ООО "СТРАДА"</company>
     <url><?php echo $GLOBALS['base_url']; ?></url>
     <currencies><currency id="RUR" rate="1"/></currencies>
