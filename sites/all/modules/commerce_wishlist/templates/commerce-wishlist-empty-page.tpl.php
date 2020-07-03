7a5d7d3437f93ecbbff4823f547c6b5364a5053a
ntym4ek <mega_comp@mail.ru>
2020-07-03 10:12:49 +0300

    reinit

---
+++ sites/all/modules/commerce_wishlist/templates/commerce-wishlist-empty-page.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * Empty wish list page template.
+ *
+ * Available variables:
+ *   $account - The user account that the wishlist belongs to.
+ */
+
+?>
+<div class="commerce-wishlist-empty">
+  <p><?php print t('There is nothing in your wish list.'); ?></p>
+</div>
